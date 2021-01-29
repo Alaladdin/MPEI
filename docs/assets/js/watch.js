7f2dadded5d95ffea344282320436d1742eb9659
Aladdin <g.tabroid@gmail.com>
2021-01-29 06:44:24 +0300

    Улучшена страница видеозаписей
    Общее:
    - Добавлены линтеры для улучшения кода: eslint, stylelint
    - Значение tag_manager в файле конфигурации mkdocs указывается через environment variable
    - Обновлен .gitignore
    
    Страница видеозаписей:
    - Получение списка видео из плейлиста на ютубе, вместо ручного указания (необходима дороботка)

--- docs/assets/js/watch.js
+++ docs/assets/js/watch.js
@@ -1,35 +1,66 @@
 {
-  
-  createDropdown('.watch__select', {
-    list: watchList,
-  });
-  
-  const watchSelect = document.querySelector('.component-dropdown__select');
-  const watchPrevBtn = document.querySelector('.watch__btn--prev');
-  const watchNextBtn = document.querySelector('.watch__btn--next');
-  
-  const tag = document.createElement('script');
-  const firstScriptTag = document.querySelector('script');
-  
   let player;
-  
-  function changeWatch(videoId) {
+
+  /**
+   * Creates error message on page
+   * @param {string} mess
+   */
+  function createErrorMess(mess) {
+    if (mess.length <= 0) return;
+    const watchError = document.querySelector('.watch__error');
+    const watchErrorMess = document.querySelector('.watch__error-mess');
+
+    watchError.classList.remove('sr-only');
+    watchErrorMess.textContent = mess;
+  }
+
+  /**
+   * Getting YouTube url
+   * @param {string, Array} part
+   * @param {string, number} maxResults
+   * @param {string} playListID
+   * @param {string} apiKey
+   * @returns {string}
+   */
+  function getUrl({
+    part = ['snippet'],
+    maxResults = 100,
+    playListID = '',
+    apiKey = 'AIzaSyCfWOrIaPhG9liEfUzmUTlv1Tl0m5D1aCE',
+  }) {
+    return `https://www.googleapis.com/youtube/v3/playlistItems?part=${part.join()}&maxResults=${maxResults}&playlistId=${playListID}&key=${apiKey}`;
+  }
+
+  /**
+   * Call API
+   * @return {Promise<any>}
+   */
+  async function apiCall() {
+    return await fetch(getUrl({
+      playListID: 'PLpjptrZp1_VedrMj81bdJQIWWNYct6lSQ',
+    })).then((res) => res.json()).catch((err) => createErrorMess(err));
+  }
+
+  /**
+   * Change player video
+   * @param {string} videoId
+   * @param {string} videoTitle
+   */
+  function changeWatch(videoId, videoTitle) {
+    const watchTitle = document.querySelector('.watch__name');
+
     player.loadVideoById({
       'videoId': videoId,
       'autoplay': 0,
     });
+
+    watchTitle.textContent = videoTitle;
   }
-  
-  const enableWatchBtn = (btn) => {
-    btn.classList.remove('watch__btn--disabled');
-    btn.disabled = false;
-  };
-  
-  const disableWatchBtn = (btn) => {
-    btn.classList.add('watch__btn--disabled');
-    btn.disabled = true;
-  };
-  
+
+  /**
+   * Creates YouTube video player
+   * @return {void}
+   */
   function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
       height: '360',
@@ -37,42 +68,34 @@
       videoId: 'dQw4w9WgXcQ',
     });
   }
-  
-  watchSelect.onchange = () => {
-    changeWatch(watchSelect.value);
-    checkBoundaries();
-  };
-  
-  tag.src = 'https://www.youtube.com/iframe_api';
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-  
-  watchPrevBtn.onclick = () => {
-    watchSelect.selectedIndex--;
-    watchSelect.dispatchEvent(new Event('change'));
-    checkBoundaries();
-  };
-  
-  watchNextBtn.onclick = () => {
-    watchSelect.selectedIndex++;
-    watchSelect.dispatchEvent(new Event('change'));
-    checkBoundaries();
-  };
-  
-  function checkBoundaries() {
-    if (watchSelect.selectedIndex === 0) {
-      disableWatchBtn(watchPrevBtn);
+
+  apiCall().then((res) => {
+    const watchList = {};
+    const watchCount = document.querySelector('.watch__count');
+    const tag = document.createElement('script');
+    const firstScriptTag = document.querySelector('script');
+
+    tag.src = 'https://www.youtube.com/iframe_api';
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    if (!res.error) {
+      for (const item of res.items) {
+        const snippet = item.snippet;
+        watchList[snippet.resourceId.videoId] = snippet.title;
+      }
+
+      createDropdown('.watch__select', {
+        list: watchList,
+      });
+
+      const watchSelect = document.querySelector('.component-dropdown__select');
+
+      watchCount.textContent = res.pageInfo.totalResults;
+      watchSelect.onchange = () => changeWatch(
+          watchSelect.value,
+          watchList[watchSelect.value],
+      );
+    } else {
+      createErrorMess(res.error.message);
     }
-    
-    if (watchSelect.selectedIndex < watchSelect.length) {
-      enableWatchBtn(watchNextBtn);
-    }
-    
-    if (watchSelect.selectedIndex === watchSelect.length - 1) {
-      disableWatchBtn(watchNextBtn);
-    }
-    
-    if (watchSelect.selectedIndex > 0) {
-      enableWatchBtn(watchPrevBtn);
-    }
-  }
+  });
 }
