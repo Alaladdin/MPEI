25402c46e71b9267c6d3e1af36f5f6467d279dc9
Aladdin <g.tabroid@gmail.com>
2021-04-02 23:40:23 +0300

    Улучшения кода
    - Улучшен код файла "main.js"

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,6 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const pwaInstall = document.querySelector('.pwa-install');
   const networkStatus = document.querySelector('.network-status');
   const networkStatusText = document.querySelector('.network-status__text');
+  let deferredPrompt;
+
+  const showNotification = (text, options) => {
+    const defaultOptions = {
+      text,
+      duration: 0,
+      gravity: 'bottom',
+      position: 'left',
+      style: {
+        fontSize: '0.8rem',
+        background: '#3d3a51',
+        userSelect: 'none',
+      },
+      stopOnFocus: true,
+    };
+    console.log(Object.assign(defaultOptions, options));
+    Toastify(Object.assign(defaultOptions, options))
+      .showToast();
+  };
 
   // update online status
   const updateOnlineStatus = () => {
@@ -9,27 +29,8 @@ document.addEventListener('DOMContentLoaded', () => {
     networkStatus.classList.toggle('online', navigator.onLine);
     networkStatusText.textContent = condition;
   };
-  const showNotification = (text, onClick) => {
-    Toastify({
-      text,
-      duration: 5000,
-      gravity: 'bottom',
-      position: 'left',
-      style: {
-        fontSize: '0.8rem',
-        background: '#302F3E',
-        userSelect: 'none',
-      },
-      stopOnFocus: true,
-      onClick,
-    })
-      .showToast();
-  };
 
   // pwa install prompt
-  let deferredPrompt;
-  const pwaInstall = document.querySelector('.pwa-install');
-
   window.addEventListener('beforeinstallprompt', (e) => {
     // Prevent from automatically showing the prompt
     e.preventDefault();
@@ -40,23 +41,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // pwa btn click event
     pwaInstall.addEventListener('click', () => {
-      pwaInstall.classList.remove('show');
 
       // Show the prompt
       deferredPrompt.prompt();
 
       // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice.then((choiceResult) => {
-        if (yaCounter71337676 && choiceResult.outcome === 'accepted') yaCounter71337676.reachGoal('pwaInstalled');
-        deferredPrompt = null;
-      });
+      deferredPrompt.userChoice
+        .then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            pwaInstall.classList.remove('show');
+            if (yaCounter71337676) yaCounter71337676.reachGoal('pwaInstalled');
+          }
+          deferredPrompt = null;
+        });
     });
   });
-
-  // events
-  window.addEventListener('online', updateOnlineStatus);
-  window.addEventListener('offline', updateOnlineStatus);
-  updateOnlineStatus();
 
   // serviceWorker
   if ('serviceWorker' in navigator) {
@@ -65,13 +64,20 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('[SW] Registered');
 
         reg.addEventListener('updatefound', () => {
-
-          // if there's an existing controller (previous Service Worker), show the prompt
           if (navigator.serviceWorker.controller) {
-            showNotification('cache update is available', () => window.location.reload());
+            showNotification('cache update is available', {
+              onClick() {
+                window.location.reload();
+              },
+            });
           }
         });
       })
       .catch(console.error);
   }
+
+  // init
+  window.addEventListener('online', updateOnlineStatus);
+  window.addEventListener('offline', updateOnlineStatus);
+  updateOnlineStatus();
 });
